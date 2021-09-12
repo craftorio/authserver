89ee8b44699c6586e803ab1f9612777b578c514d
scherepanov <sergey@cherepanov.org.ua>
2021-09-12 18:31:59 +0400

    refresh token fix

--- authserver/src/Route/Refresh.php
+++ authserver/src/Route/Refresh.php
@@ -67,7 +67,7 @@ class Refresh implements RouteInterface
             ['clientToken', '=', $clientToken],
         ]);
 
-        if (!$session) {
+        if (!$session || !$session['accountId']) {
             \Flight::json([
                 "error" => "ForbiddenOperationException", // Package ca.uhn.fhir.rest.server.exceptions
                 "errorMessage" => "Invalid credentials. Invalid username or password."
