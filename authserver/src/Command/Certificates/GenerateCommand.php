45f51bca0d2f8a3853e3ed0fcef0cb45b0049925
Workspace Owner <scherepanov@scherepanov-mc-auth.cherepanov.local>
2024-02-26 15:15:01 +0000

    skin

--- authserver/src/Command/Certificates/GenerateCommand.php
+++ authserver/src/Command/Certificates/GenerateCommand.php
@@ -46,6 +46,7 @@ class GenerateCommand extends Command
     {
         // Generate the key
         $key = openssl_pkey_new([
+            "digest_alg" => 'sha1',
             "private_key_bits" => 4096,
             "private_key_type" => OPENSSL_KEYTYPE_RSA,
         ]);
